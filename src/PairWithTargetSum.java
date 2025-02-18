b6778ced8dc23b0630910530cc43f093dd12af41
Iredafe Owolabi <iowolabi@westjet.com>
2025-02-18 14:38:45 -0700

    pair II

--- src/PairWithTargetSum.java
+++ src/PairWithTargetSum.java
@@ -1,7 +1,5 @@
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class PairWithTargetSum {
 
@@ -19,7 +17,7 @@ public class PairWithTargetSum {
             } else if (sum < target) {
                 i++;
             } else {
-                result.add(new int[]{arr[i], arr[j]});
+                result.add(new int[]{i, j});
                 i++;
                 j--;
             }
@@ -30,7 +28,33 @@ public class PairWithTargetSum {
 
     }
 
+    private static List<int []> pairWithTargetSumHashMap(int [] arr, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        List<int[]> result = new ArrayList<>();
 
+        for(int i = 0; i < arr.length; i++) {
+            Integer complement = target - arr[i];
+            if(map.containsKey(complement)) {
+                result.add(new int[]{map.get(complement), i});
+            }else{
+                map.put(arr[i], i);
+            }
+        }
+
+        return result;
+    }
+
+    public static int[] search(int[] arr, int targetSum) {
+        HashMap<Integer, Integer> nums = new HashMap<>(); // to store numbers and indices
+        for (int i = 0; i < arr.length; i++) {
+            int complement = targetSum - arr[i];
+            if (nums.containsKey(complement))
+                return new int[] { nums.get(complement), i };
+            else
+                nums.put(arr[i], i); // put the number and its index in the map
+        }
+        return new int[] { -1, -1 }; // pair not found
+    }
     public static void main(String[] args) {
         int [] arr= new int [] {1, 2, 3, 4, 6};
         int target = 6;
@@ -39,6 +63,15 @@ public class PairWithTargetSum {
         for (int[] pair : result) {
             System.out.println(Arrays.toString(pair));
         }
+        List<int[]> result1 = pairWithTargetSumHashMap(arr, target);
+        System.out.println("Pairs with sum hashmap: " + target + ":");
+        for (int[] pair : result1) {
+            System.out.println(Arrays.toString(pair));
+        }
+        int[] result2 = search(new int[] { 1, 2, 3, 4, 6 }, 6);
+        System.out.println("Pair with target sum: [" + result2[0] + ", " + result2[1] + "]");
+        result2 = search(new int[] { 2, 5, 9, 11 }, 11);
+        System.out.println("Pair with target sum: [" + result2[0] + ", " + result2[1] + "]");
 
     }
 }
