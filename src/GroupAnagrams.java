6046b565d3e3a4833d4529a10853421128bd4d44
Iredafe Owolabi <iowolabi@westjet.com>
2025-02-19 09:21:52 -0700

    group anagrams

--- src/GroupAnagrams.java
+++ src/GroupAnagrams.java
@@ -1,2 +1,27 @@
+import java.util.*;
+
 public class GroupAnagrams {
+
+    private static List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String word : strs){
+            char [] sortedWord = word.toCharArray();
+            Arrays.sort(sortedWord);
+            String sortedKey = new String (sortedWord);
+            if(map.containsKey(sortedKey)){
+               map.get(sortedKey).add((word));
+            }else{
+                map.put(sortedKey, new ArrayList<>(List.of(word)));
+            }
+        }
+
+
+        return new ArrayList<>(map.values());
+    }
+
+    public static void main(String[] args) {
+        String[] words = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        List<List<String>> result = groupAnagrams(words);
+        System.out.println(result);
+    }
 }
